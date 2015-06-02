a06cc044558684708b2718406f676efb8334de40
José Manuel Marrón <jose@bldo.com>
2015-06-02 10:36:20 -0500

    Remove connect call

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -56,8 +56,6 @@ module.exports.bootstrap = function (cb) {
         console.log("APN error: " + errorCode);
     });
 
-    sails.apnConnection.connect();
-
     // It's very important to trigger this callback method when you are finished
     // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
     cb();
